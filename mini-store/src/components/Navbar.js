666753ea14cdd7a07ec5202ff1b973368cf3b87f
Padmakumar18 <padmakumar41759@gmail.com>
2025-05-10 17:20:10 +0530

    Code updated

--- mini-store/src/components/Navbar.js
+++ mini-store/src/components/Navbar.js
@@ -1,15 +1,20 @@
-// src/components/Navbar.js
+// Navbar.js
 import React from 'react';
 import './Navbar.scss';
+import { FaShoppingCart, FaUserCircle } from 'react-icons/fa';
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <a href='/' className="navbar__logo">இ - Store</a>
+      <a href='/' className="navbar__logo">QuickBuy</a>
       <input type="text" placeholder="Search..." className="navbar__search" />
       <div className="navbar__actions">
-        <button className="navbar__btn">Cart</button>
-        <button className="navbar__btn">Profile</button>
+        <button className="navbar__btn">
+          <FaShoppingCart size={15} /> Cart
+        </button>
+        <button className="navbar__btn">
+          <FaUserCircle size={15} /> Profile
+        </button>
       </div>
     </nav>
   );
