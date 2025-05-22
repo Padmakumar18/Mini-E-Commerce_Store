00d2452fb0dc6074662598da6362005b5e298deb
Padmakumar18 <padmakumar41759@gmail.com>
2025-05-22 09:26:55 +0530

    Categories created and  footre file created

--- mini-store/src/components/Home.js
+++ mini-store/src/components/Home.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import "./Home.scss";
 
 function Home() {
   return (
-    <div>
+    <div className="home">
+      <div className="sliding">
+        <p>Here sliding the image</p>
+      </div>
+      <div className="categories">
+        <button>Electronics</button>
+        <button>Fashion</button>
+        <button>Home & Kitchen</button>
+        <button>Beauty & Personal Care</button>
+        <button>Health & Wellness</button>
+        <button>Books & Stationery</button>
+        <button>Sports & Outdoors</button>
+        <button>Jewelry & Watches</button>
+      </div>
+
+      <div className="randomProducts">
         <div className="sliding">
-          <p>Here sliding the image</p>
+          <p>Random products</p>
         </div>
+      </div>
     </div>
   );
 }
--- mini-store/src/components/Navbar.js
+++ mini-store/src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <nav className="navbar">
       <a href='/' className="navbar__logo">QuickBuy</a>
-      <input type="text" placeholder="Search..." className="navbar__search" />
+      <input type="text" placeholder="Search here..." className="navbar__search" />
       <div className="navbar__actions">
         <button className="navbar__btn">
           <FaShoppingCart size={15} /> Cart
