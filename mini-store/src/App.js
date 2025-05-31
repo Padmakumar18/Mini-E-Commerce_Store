16079168fd2dc7c29072e1cd44bc55ef4916006a
Padmakumar18 <padmakumar41759@gmail.com>
2025-05-31 14:36:21 +0530

    Code updated

--- mini-store/src/App.js
+++ mini-store/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div>
       {/* <Login/> */}
       <Navbar />
-      <Home />
+      {/* <Home /> */}
 
       <Profile/>
     </div>
--- mini-store/src/components/Profile.js
+++ mini-store/src/components/Profile.js
@@ -1,9 +1,22 @@
-import './Profile.scss';
+import "./Profile.scss";
 
 function Profile() {
   return (
-    <div className='container'>
-        
+    <div className="profile-section">
+      <div className="first-section">
+        <div className="profile-photo">
+          <p>Here photos</p>
+        </div>
+        <div className="user-details">
+          <p>Padmakumar</p>
+          <p>padmakumar@gmail.com</p>
+          <p>9944035088</p>
+        </div>
+      </div>
+
+			<div className="section">
+				<p>Total orders : 10</p>
+			</div>
     </div>
   );
 }
